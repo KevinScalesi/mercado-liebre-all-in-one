259de27467d1699c3339786401caf21d6d1ba5fd
KevinScalesi <kevin_scalesi@yahoo.com.ar>
2021-01-14 21:00:42 -0300

    punto 3 ejercitación 1 APIS

--- public/js/products.js
+++ public/js/products.js
@@ -4,25 +4,52 @@ window.addEventListener("load", function(){
             return res.json()
         })
         .then(latest => {
-            let ultimos = document.querySelectorAll("section.product-box")
+            let a = document.querySelectorAll("a.linkLatest")
+            let img = document.querySelectorAll("img.imgLatest")
+            let h2 = document.querySelectorAll("h2.priceLatest")
+            let span = document.querySelectorAll("span.discountLatest")
+            let p = document.querySelectorAll("p.nameLatest")
+
             for(let i=0; i<latest.data.length;i++){
-                ultimos[i].firstElementChild.href = `/products/detail/${latest.data[i].id}`
-                ultimos[i].firstElementChild.firstElementChild.firstElementChild.src = `/images/products/${latest.data[i].image}`
-                ultimos[i].firstElementChild.firstElementChild.firstElementChild.alt = `${latest.data[i].name}`
-            };
-            // let a = document.querySelectorAll("a.prodId")
-            // let img = document.querySelectorAll("img.imgProd")
-            
-           console.log(ultimos)   
-               
-            //         ultimos.h2.innerHTML = `$${helpers.trunc(product.price - product.price * product.discount / 100)}`
-            //         ultimos.span.innerHTML = `${ product.discount} % OFF`
-            //         ultimos.p.innerHTML = `${product.name}`
-            //     });
-            // }
-            console.log(latest.data)
+               a.href = `/products/detail/${latest.data[i].id}`
+               img.src = `/images/products/${latest.data[i].image}`
+               img.alt = `${latest.data[i].name}`
+               h2.innerHTML = `$ ${latest.data[i].price - latest.data[i].price * latest.data[i].discount / 100}`
+               if(latest.data[i].discount > 0){
+                   span.innerHTML = `${ latest.data[i].discount} % OFF`
+               }
+               p.innerHTML = `${latest.data[i].name}`
+            }; 
         })
         .catch(error=>{
             console.log(error)
         })
+
+    fetch("http://localhost:3000/api/products/oferts")
+    .then(function(res){
+        return res.json()
+    })
+    .then(oferts=>{
+        console.log(oferts)
+        let a = document.querySelectorAll("a.linkOferts")
+        let img = document.querySelectorAll("img.imgOferts")
+        let h2 = document.querySelectorAll("h2.priceOferts")
+        let span = document.querySelectorAll("span.discountOferts")
+        let p = document.querySelectorAll("p.nameOferts")
+
+        for(let i=0; i< oferts.data.length;i++){
+           a.href = `/products/detail/${oferts.data[i].id}`
+           img.src = `/images/products/${oferts.data[i].image}`
+           img.alt = `${oferts.data[i].name}`
+           h2.innerHTML = `$ ${oferts.data[i].price - oferts.data[i].price * oferts.data[i].discount / 100}`
+           if(oferts.data[i].discount > 0){
+               span.innerHTML = `${ oferts.data[i].discount} % OFF`
+           }
+           p.innerHTML = `${oferts.data[i].name}`
+        }; 
+    })
+    .catch(error=>{
+        console.log(error)
+    })
+
 })
